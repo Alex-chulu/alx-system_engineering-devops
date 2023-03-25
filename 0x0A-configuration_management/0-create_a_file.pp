920ae8e3c9ede33fd01ce08202c265f29b3526a5
Alex-chulu <chulualexandar@gmail.com>
2023-03-25 06:13:05 -0700

    Create a file fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 #This manifest creates a file in /tmp directory with the name school
 file { '/tmp/school':
-        ensure  => present,
-        mode    => 0744,
+        ensure  => file,
+        mode    => '0744',
         owner   => 'www-data',
         group   => 'www-data',
         content => 'I love Puppet',
-        }
 }
