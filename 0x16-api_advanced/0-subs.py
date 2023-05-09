5b8d88c0355136171f1af08bef13299d308667ed
Alex-Chulu <chulualexandar@gmail.com>
2023-05-09 15:01:22 +0000

    task 0 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
+
 import requests
 
 def number_of_subscribers(subreddit):
-    url = f("https://www.reddit.com/r/{subreddit}/about.json")
-    headers = {"User-Agent": "myBot/0.0.1"}
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers for the subreddit. Returns 0 if the subreddit is invalid.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "MyAPI/0.0.1"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json()
         return data["data"]["subscribers"]
     else:
         return 0
-
