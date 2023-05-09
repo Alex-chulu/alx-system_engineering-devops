f5d662b78954104ac2c2dcf5b3b40558373d2282
Alex-Chulu <chulualexandar@gmail.com>
2023-05-09 16:55:46 +0000

    task 1 fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,7 +4,8 @@ import requests
 
 def top_ten(subreddit):
     """
-    Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+    Queries the Reddit API and prints the titles of the first 
+    10 hot posts listed for a given subreddit.
     
     Args:
         subreddit (str): The name of the subreddit to query.
