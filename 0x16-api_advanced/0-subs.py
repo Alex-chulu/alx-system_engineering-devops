e58407efaaa3c815528dbd7925465c403c82cc2f
Alex-Chulu <chulualexandar@gmail.com>
2023-05-10 02:54:28 +0000

    task 0 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,7 +19,7 @@ def number_of_subscribers(subreddit):
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'My API Request Agent'}
-    res = requests.get(url, headers=headers)
+    res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
         data = res.json()
         return data.get('data').get('subscribers')
