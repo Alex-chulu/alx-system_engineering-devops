3c44716d338e8b571cfcde7e981ce88990576b20
Alex-Chulu <chulualexandar@gmail.com>
2023-05-10 02:00:38 +0000

    task 1 fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
+""" Function that queries the Reddit API """
 import requests
+import sys
 
 
 def top_ten(subreddit):
     """
-    Queries the Reddit API and prints the titles of the first 
-    10 hot posts listed for a given subreddit.
-    
+    Queries the Reddit API and prints the titles of the first
+    10 hot posts for a given subreddit.
+
     Args:
         subreddit (str): The name of the subreddit to query.
-        
+
     Returns:
         None
     """
-    url = "https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'MyApi'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'limit': 10}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
 
-    if response.status_code != 200:
+    if response.status_code == 200:
+        dat = response.json().get('data').get('children')
+        for result in results:
+            print(data.get('data').get('title'))
+    else:
         print(None)
-        return
-
-    data = response.json()["data"]["children"]
-
-    for post in data:
-        print(post["data"]["title"])
-
