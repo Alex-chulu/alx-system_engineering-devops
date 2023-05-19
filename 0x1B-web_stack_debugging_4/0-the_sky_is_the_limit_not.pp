274c2472336af694616c3e430bffca71a1f8abb9
Alex-Chulu <chulualexandar@gmail.com>
2023-05-19 15:47:10 +0000

    task 0 fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #Fixing nginx so that it should serve 2000 requests
-exec { 'increase_nginx':
+exec { 'increase-nginx':
   command => '/usr/bin/env sed -i s/4/2500/ /etc/default/nginx',
 }
 exec { '/usr/bin/env service nginx restart': }
