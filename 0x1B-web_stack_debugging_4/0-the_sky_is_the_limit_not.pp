843190d1750bc3b881b7193b6e3b99b9e780e50e
Alex-Chulu <chulualexandar@gmail.com>
2023-05-19 15:22:22 +0000

    0-the_sky_is_the_limit_not

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+#Fixing nginx so that it should serve 2000 requests
+exec { 'increase_nginx_limit':
+  command => 'sed -i "s/4/2500/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
+}
+
+#Nginx restarting
+exec { 'restart_nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
